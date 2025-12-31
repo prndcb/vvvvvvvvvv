28b19048f7cd2b4af850e2781fb64c9e09827ed6
prndcb <psatanique@gmail.com>
2025-12-31 04:22:27 +0100

    Fix presence activity URL and add comment
    Updated the presence activity URL and added comments.

--- Events/clientReady.js
+++ Events/clientReady.js
@@ -1,12 +1,18 @@
 import { ActivityType } from "discord.js";
 
 export default {
-	name: 'clientReady',
-	async execute(bot) {
-		await bot.application.commands.set(bot.arrayOfSlashCommands);
+  name: "clientReady",
+  async execute(bot) {
+    await bot.application.commands.set(bot.arrayOfSlashCommands);
 
-		bot.user.setPresence({
-			activities: [{ name: 'Substance', type: ActivityType.Streaming, url: 'https://www.tiktok.com/@missirelia?_r=1&_t=ZN-92fcGQXZvUs' }], status: 'online'
-		});
-	}
+    // Activité Streaming avec lien TikTok
+    bot.user.setPresence({
+      activities: [{
+        name: "Substance",
+        type: ActivityType.Streaming,
+        url: "https://www.tiktok.com/@missirelia"
+      }],
+      status: "online"
+    });
+  }
 };
