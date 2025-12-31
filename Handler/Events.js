2f0d17f3e2c73b4d6e4ddcea09c7f3de40d8330d
prndcb <psatanique@gmail.com>
2025-12-31 04:37:16 +0100

    Refactor event loading in Events.js

--- Handler/Events.js
+++ Handler/Events.js
@@ -1,34 +1,42 @@
-import config from "../config.json" with { type: 'json' }
-import fs from "fs"
+import fs from "fs";
+import config from "../config.json" with { type: "json" };
 
 export default async (bot) => {
-	const eventFiles = fs.readdirSync('./Events/').filter((file) => file.endsWith('.js'));
+  // ============================
+  //  Charger les fichiers events
+  // ============================
+  const eventFiles = fs.readdirSync("./Events/").filter((file) => file.endsWith(".js"));
 
-	for (const file of eventFiles) {
-		const event = (await import(`../Events/${file}`)).default;
+  for (const file of eventFiles) {
+    const event = (await import(`../Events/${file}`)).default;
 
-		if (event.once) {
-			bot.once(event.name, (...args) => event.execute(...args, bot, config));
-		} else {
-			bot.on(event.name, (...args) => event.execute(...args, bot, config));
-		}
-		console.log(`[EVENT] > ${file}`);
-	}
+    if (event.once) {
+      bot.once(event.name, (...args) => event.execute(...args, bot, config));
+    } else {
+      bot.on(event.name, (...args) => event.execute(...args, bot, config));
+    }
 
-	const eventSubFolders = fs.readdirSync('./Events/').filter((folder) => !folder.endsWith('.js'));
+    console.log(`[EVENT] > ${file}`);
+  }
 
-	for (const folder of eventSubFolders) {
-		const subEventFiles = fs.readdirSync(`./Events/${folder}/`).filter((file) => file.endsWith('.js'));
+  // =====================================
+  //  Charger les sous-dossiers Events
+  // =====================================
+  const eventSubFolders = fs.readdirSync("./Events/").filter((folder) => !folder.endsWith(".js"));
 
-		for (const file of subEventFiles) {
-			const event = (await import(`../Events/${folder}/${file}`)).default;
+  for (const folder of eventSubFolders) {
+    const subEventFiles = fs.readdirSync(`./Events/${folder}/`).filter((file) => file.endsWith(".js"));
 
-			if (event.once) {
-				bot.once(event.name, (...args) => event.execute(...args, bot, config));
-			} else {
-				bot.on(event.name, (...args) => event.execute(...args, bot, config));
-			}
-			console.log(`[EVENT] > ${file} - ${folder}`);
-		}
-	}
+    for (const file of subEventFiles) {
+      const event = (await import(`../Events/${folder}/${file}`)).default;
+
+      if (event.once) {
+        bot.once(event.name, (...args) => event.execute(...args, bot, config));
+      } else {
+        bot.on(event.name, (...args) => event.execute(...args, bot, config));
+      }
+
+      console.log(`[EVENT] > ${file} - ${folder}`);
+    }
+  }
 };
